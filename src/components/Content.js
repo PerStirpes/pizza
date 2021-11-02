9d852f501841a859db4062fe9c3cd73fe6b4d770
PerStirpes <mccarl@asu.edu>
2021-11-02 17:14:16 -0400

    fix ul li issue

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,33 +1,36 @@
 import React, { Component } from "react"
-
-// import { Row, Col } from "reactstrap"
-
 import products from "../utils/contentData"
+import Button from "./Button"
 
 class Content extends Component {
     render() {
         return (
-            <ul>
-                <h2 className="title">Hungry for a Jumbo Slice</h2>
-                <li className="grid">
-                    {products.map((product, i) => {
-                        const { link, title, image, description, price } = product
-                        return (
-                            <li key={i} className="card">
-                                <a href={link}>
-                                    <img src={image} />
-                                    <br />
-                                    <h3> {title}</h3>
+            <div className="containers">
+                <h1 className="title">Pizza 42 POC</h1>
+                <p className="description">Hungry for a Jumbo Slice? The Best Jumbo Slice in Your Town!</p>
+                <main className="mains">
+                    <ul className="grid">
+                        {products.map((product, i) => {
+                            const { link, title, image, description, price } = product
+                            return (
+                                <li key={i} className="card">
+                                    <a href={link}>
+                                        <img src={image} alt={title} />
+                                        <br />
+                                        <h3> {title}</h3>
 
-                                    <p> {price}</p>
-                                </a>
-
-                                <p>{description}</p>
-                            </li>
-                        )
-                    })}
-                </li>
-            </ul>
+                                        <p> {price}</p>
+                                    </a>
+                                    <p>
+                                        <Button />
+                                    </p>
+                                    <p>{description}</p>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </main>
+            </div>
         )
     }
 }
